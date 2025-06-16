a7544187411787fc4a8168912236dbb5ef58797a
SantiagoT117 <148306605+SantiagoT117@users.noreply.github.com>
2025-06-16 18:18:27 +0000

    Commit 2 Arreglar y configurar todo pa que funcione

---
+++ build/CMakeFiles/pruebas.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/ProyectoFinalDefinitivo/Pruebas/main.cpp" "/workspaces/ProyectoFinalDefinitivo/build/CMakeFiles/pruebas.dir/main.cpp.o"
+  "/workspaces/ProyectoFinalDefinitivo/Pruebas/src/Ahorcado.cpp" "/workspaces/ProyectoFinalDefinitivo/build/CMakeFiles/pruebas.dir/src/Ahorcado.cpp.o"
+  "/workspaces/ProyectoFinalDefinitivo/Pruebas/src/Juegos.cpp" "/workspaces/ProyectoFinalDefinitivo/build/CMakeFiles/pruebas.dir/src/Juegos.cpp.o"
+  "/workspaces/ProyectoFinalDefinitivo/Pruebas/src/Jugadores.cpp" "/workspaces/ProyectoFinalDefinitivo/build/CMakeFiles/pruebas.dir/src/Jugadores.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/ProyectoFinalDefinitivo/Pruebas/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
